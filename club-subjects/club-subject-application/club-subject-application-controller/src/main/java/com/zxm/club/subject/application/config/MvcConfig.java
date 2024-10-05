041ce097cddd7199d9da06257bdfb3d44599168f
zzb <2048433880@qq.com>
2024-10-05 12:15:49 +0800

    刷题模块完成

--- club-subjects/club-subject-application/club-subject-application-controller/src/main/java/com/zxm/club/subject/application/config/MvcConfig.java
+++ club-subjects/club-subject-application/club-subject-application-controller/src/main/java/com/zxm/club/subject/application/config/MvcConfig.java
@@ -1,18 +1,31 @@
 package com.zxm.club.subject.application.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.List;
 
-//@Component
+@Component
 public class MvcConfig extends WebMvcConfigurationSupport {
-//    @Override
-//    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//        super.configureMessageConverters(converters);
-//        converters.add(null);
-//    }
+    //返回给前端的时候，值为null的属性不进行序列化
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 配置不序列化 null 属性
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+
 
 
 }
